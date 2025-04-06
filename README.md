3d5b49b73daa22bd4b1192ebeab4f12796237a20
ishaan766 <ishaan766@gmail.com>
2025-04-06 23:05:03 +0530

    # Go net/http Instrumentation

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
-# Go net/http OpenTelemetry Instrumentation
+# Go net/http Instrumentation
 
-This is a sample app to demonstrate how to instrument Go app using net/http with OpenTelemetry. It contains source code for the Go app which interacts with various services like Redis, MySQL, MongoDB, Kafka, ClickHouse, etc. to demonstrate tracing for these services. This repository has a docker compose file to set up all these services conveniently.
+This is a sample app to demonstrate how to instrument Go app using net/http with **Datadog**, **Elastic**, **New Relic** and **OpenTelemetry**. It contains source code for the Go app which interacts with various services like Redis, MySQL, MongoDB, Kafka, ClickHouse, etc. to demonstrate tracing for these services. This repository has a docker compose file to set up all these services conveniently.
 
-This repository is inentionally designed to work with any OpenTelemetry backend, not just CubeAPM. In fact, it can even work without any OpenTelemetry backend (by dumping traces to console, which is also the default behaviour).
+The code is organized into multiple branches. The main branch has the Go net/http app without any instrumentation. Other branches then build upon the main branch to add specific instrumentations as below:
+
+| Branch                                                                                         | Instrumentation | Code changes for instrumentation                                                                                |
+| ---------------------------------------------------------------------------------------------- | --------------- | --------------------------------------------------------------------------------------------------------------- |
+| [main](https://github.com/cubeapm/sample_app_go_net_http/tree/main)         | None            | -                                                                                                               |
+| [datadog](https://github.com/cubeapm/sample_app_go_net_http/tree/datadog) | Datadog       | [main...datadog](https://github.com/cubeapm/sample_app_go_net_http/compare/main...datadog) |
+| [elastic](https://github.com/cubeapm/sample_app_go_net_http/tree/elastic)         | Elastic   | [main...elastic](https://github.com/cubeapm/sample_app_go_net_http/compare/main...elastic)         |
+| [newrelic](https://github.com/cubeapm/sample_app_go_net_http/tree/newrelic) | New Relic       | [main...newrelic](https://github.com/cubeapm/sample_app_go_net_http/compare/main...newrelic) |
+| [otel](https://github.com/cubeapm/sample_app_go_net_http/tree/otel)         | OpenTelemetry   | [main...otel](https://github.com/cubeapm/sample_app_go_net_http/compare/main...otel)         |
 
 ## Setup
 
@@ -14,8 +22,8 @@ docker compose up --build
 
 Go app will now be available at `http://localhost:8000`.
 
-The app has various API endpoints to demonstrate OpenTelemetry integrations with Redis, MySQL, MongoDB, Kafka, ClickHouse, etc. Check out [main.go](main.go) for the list of API endpoints. Hitting an API endpoint will generate the corresponding traces. Traces are printed to console (where docker compose is running) by default. If you want to send traces to a backend tool, comment out the `OTEL_LOG_LEVEL` line and uncomment the `OTEL_EXPORTER_OTLP_TRACES_ENDPOINT` line in [docker-compose.yml](docker-compose.yml).
+The app has various API endpoints to demonstrate OpenTelemetry integrations with Redis, MySQL, MongoDB, Kafka, ClickHouse, etc. Check out [main.go](main.go) for the list of API endpoints.
 
-## Contributing
+# Contributing
 
 Please feel free to raise PR for any enhancements - additional service integrations, library version updates, documentation updates, etc.
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -14,24 +13,16 @@ import (
 
 	"github.com/ClickHouse/clickhouse-go/v2"
 	"github.com/ClickHouse/clickhouse-go/v2/lib/driver"
-	"github.com/redis/go-redis/extra/redisotel/v9"
 	"github.com/redis/go-redis/v9"
 	"github.com/segmentio/kafka-go"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
-	"go.opentelemetry.io/contrib/instrumentation/go.mongodb.org/mongo-driver/mongo/otelmongo"
-	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
-	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/codes"
-	semconv "go.opentelemetry.io/otel/semconv/v1.25.0"
-	apiTrace "go.opentelemetry.io/otel/trace"
 )
 
 const kafkaTopicName = "sample_topic"
 
-var tracer apiTrace.Tracer
 var hcl http.Client
 var rdb *redis.Client
 var mdb *mongo.Client
@@ -46,23 +37,15 @@ func main() {
 
 func run() (err error) {
 	// initialize http client
-	hcl = http.Client{
-		Transport: otelhttp.NewTransport(http.DefaultTransport),
-	}
+	hcl = http.Client{}
 
 	// initialize redis
 	rdb = redis.NewClient(&redis.Options{
 		Addr: "redis:6379",
 	})
-	// Enable tracing instrumentation
-	if err := redisotel.InstrumentTracing(rdb); err != nil {
-		return err
-	}
 
 	// initialize mongo
 	mdbOpts := options.Client()
-	// Enable tracing instrumentation
-	mdbOpts.Monitor = otelmongo.NewMonitor()
 	mdbOpts.ApplyURI("mongodb://mongo:27017")
 	mdb, err = mongo.Connect(context.Background(), mdbOpts)
 	if err != nil {
@@ -97,19 +80,6 @@ func run() (err error) {
 	// Handle SIGINT (CTRL+C) gracefully.
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer stop()
-
-	// Set up OpenTelemetry.
-	otelShutdown, err := setupOTelSDK(ctx)
-	if err != nil {
-		return
-	}
-	// Handle shutdown properly so nothing leaks.
-	defer func() {
-		err = errors.Join(err, otelShutdown(context.Background()))
-	}()
-
-	// initialize tracer
-	tracer = otel.Tracer(os.Getenv("OTEL_SERVICE_NAME"))
 
 	// Start HTTP server.
 	srv := &http.Server{
@@ -146,17 +116,7 @@ func newHTTPHandler() http.Handler {
 	// handleFunc is a replacement for mux.HandleFunc
 	// which enriches the handler's HTTP instrumentation.
 	handleFunc := func(pattern string, handlerFunc func(http.ResponseWriter, *http.Request)) {
-		// Configure the "http.route" for the HTTP instrumentation.
-		handler := otelhttp.WithRouteTag(
-			pattern, http.HandlerFunc(handlerFunc),
-		)
-		handler = otelhttp.NewHandler(
-			handler, pattern,
-			otelhttp.WithSpanNameFormatter(func(operation string, r *http.Request) string {
-				return r.Method + " " + operation
-			}),
-		)
-		mux.Handle(pattern, handler)
+		mux.HandleFunc(pattern, handlerFunc)
 	}
 
 	// Register handlers.
@@ -217,38 +177,16 @@ func mongoFunc(w http.ResponseWriter, r *http.Request) {
 }
 
 func clickhouseFunc(w http.ResponseWriter, r *http.Request) {
-	_, span := tracer.Start(r.Context(), "SELECT <dbname>.<tablename>", apiTrace.WithSpanKind(apiTrace.SpanKindClient))
-	span.SetAttributes(
-		semconv.DBSystemClickhouse,
-		// semconv.DBName(""),
-		semconv.DBOperation("SELECT"),
-		// semconv.DBSQLTable(""),
-		// semconv.DBStatement(""),
-	)
-	defer span.End()
 
 	res, err := ccn.Query(r.Context(), "SELECT NOW()")
 	if err != nil {
-		span.RecordError(err)
-		span.SetStatus(codes.Error, err.Error())
+		fmt.Fprintf(w, "Clickhouse query error: %v", err)
 	}
 
 	fmt.Fprintf(w, "Clickhouse called: %v", res.Columns())
 }
 
 func kafkaProduceFunc(w http.ResponseWriter, r *http.Request) {
-	_, span := tracer.Start(r.Context(), "publish "+kafkaTopicName, apiTrace.WithSpanKind(apiTrace.SpanKindProducer))
-	span.SetAttributes(
-		semconv.MessagingSystemKafka,
-		semconv.MessagingOperationPublish,
-		semconv.MessagingDestinationName(kafkaTopicName),
-		// semconv.ServerAddress(""),
-		// semconv.MessagingKafkaMessageKey(""),
-		// semconv.MessagingMessageBodySize(14),
-		// semconv.MessagingBatchMessageCount(3),
-		// attribute.String("key", "value"), // "go.opentelemetry.io/otel/attribute"
-	)
-	defer span.End()
 
 	kcn.SetWriteDeadline(time.Now().Add(10 * time.Second))
 	_, err := kcn.WriteMessages(
@@ -257,21 +195,13 @@ func kafkaProduceFunc(w http.ResponseWriter, r *http.Request) {
 		kafka.Message{Value: []byte("three!")},
 	)
 	if err != nil {
-		span.RecordError(err)
-		span.SetStatus(codes.Error, err.Error())
+		fmt.Fprintf(w, "Kafka produce error: %v", err)
 	}
 
 	fmt.Fprintf(w, "Kafka produced")
 }
 
 func kafkaConsumeFunc(w http.ResponseWriter, r *http.Request) {
-	_, span := tracer.Start(r.Context(), "process "+kafkaTopicName, apiTrace.WithSpanKind(apiTrace.SpanKindConsumer))
-	span.SetAttributes(
-		semconv.MessagingSystemKafka,
-		semconv.MessagingOperationDeliver,
-		semconv.MessagingDestinationName(kafkaTopicName),
-	)
-	defer span.End()
 
 	kcn.SetReadDeadline(time.Now().Add(10 * time.Second))
 	_ = kcn.ReadBatch(10e3, 1e6) // fetch 10KB min, 1MB max
