e39052894129ad5e886197dee878b388dd33eafd
ishaan766 <ishaan766@gmail.com>
2025-04-06 23:44:35 +0530

    # OpenTelemetry Instrumentation

--- go.mod
+++ go.mod
@@ -4,38 +4,58 @@ go 1.23.3
 
 require (
 	github.com/ClickHouse/clickhouse-go/v2 v2.30.0
-	github.com/redis/go-redis/v9 v9.7.0
+	github.com/redis/go-redis/extra/redisotel/v9 v9.7.3
+	github.com/redis/go-redis/v9 v9.7.3
 	github.com/segmentio/kafka-go v0.4.47
-	go.mongodb.org/mongo-driver v1.17.1
+	go.mongodb.org/mongo-driver v1.17.3
+	go.opentelemetry.io/contrib/instrumentation/go.mongodb.org/mongo-driver/mongo/otelmongo v0.60.0
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.49.0
+	go.opentelemetry.io/otel v1.35.0
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.19.0
+	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.35.0
+	go.opentelemetry.io/otel/sdk v1.35.0
+	go.opentelemetry.io/otel/trace v1.35.0
 )
 
 require (
 	github.com/ClickHouse/ch-go v0.61.5 // indirect
 	github.com/andybalholm/brotli v1.1.1 // indirect
+	github.com/cenkalti/backoff/v4 v4.2.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/go-faster/city v1.0.1 // indirect
 	github.com/go-faster/errors v0.7.1 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/uuid v1.6.0 // indirect
-	github.com/klauspost/compress v1.17.11 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.16.0 // indirect
+	github.com/klauspost/compress v1.18.0 // indirect
 	github.com/montanaflynn/stats v0.7.1 // indirect
 	github.com/paulmach/orb v0.11.1 // indirect
 	github.com/pierrec/lz4/v4 v4.1.21 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/rogpeppe/go-internal v1.13.1 // indirect
+	github.com/redis/go-redis/extra/rediscmd/v9 v9.7.3 // indirect
 	github.com/segmentio/asm v1.2.0 // indirect
 	github.com/shopspring/decimal v1.4.0 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
-	go.opentelemetry.io/otel v1.33.0 // indirect
-	go.opentelemetry.io/otel/trace v1.33.0 // indirect
-	golang.org/x/crypto v0.31.0 // indirect
+	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.19.0 // indirect
+	go.opentelemetry.io/otel/metric v1.35.0 // indirect
+	go.opentelemetry.io/proto/otlp v1.0.0 // indirect
+	golang.org/x/crypto v0.33.0 // indirect
 	golang.org/x/net v0.32.0 // indirect
-	golang.org/x/sync v0.10.0 // indirect
-	golang.org/x/sys v0.28.0 // indirect
-	golang.org/x/text v0.21.0 // indirect
+	golang.org/x/sync v0.11.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240318140521-94a12d6c2237 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240318140521-94a12d6c2237 // indirect
+	google.golang.org/grpc v1.62.1 // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -13,16 +14,24 @@ import (
 
 	"github.com/ClickHouse/clickhouse-go/v2"
 	"github.com/ClickHouse/clickhouse-go/v2/lib/driver"
+	"github.com/redis/go-redis/extra/redisotel/v9"
 	"github.com/redis/go-redis/v9"
 	"github.com/segmentio/kafka-go"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"go.opentelemetry.io/contrib/instrumentation/go.mongodb.org/mongo-driver/mongo/otelmongo"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/codes"
+	semconv "go.opentelemetry.io/otel/semconv/v1.25.0"
+	apiTrace "go.opentelemetry.io/otel/trace"
 )
 
 const kafkaTopicName = "sample_topic"
 
+var tracer apiTrace.Tracer
 var hcl http.Client
 var rdb *redis.Client
 var mdb *mongo.Client
@@ -37,15 +46,23 @@ func main() {
 
 func run() (err error) {
 	// initialize http client
-	hcl = http.Client{}
+	hcl = http.Client{
+		Transport: otelhttp.NewTransport(http.DefaultTransport),
+	}
 
 	// initialize redis
 	rdb = redis.NewClient(&redis.Options{
 		Addr: "redis:6379",
 	})
+	// Enable tracing instrumentation
+	if err := redisotel.InstrumentTracing(rdb); err != nil {
+		return err
+	}
 
 	// initialize mongo
 	mdbOpts := options.Client()
+	// Enable tracing instrumentation
+	mdbOpts.Monitor = otelmongo.NewMonitor()
 	mdbOpts.ApplyURI("mongodb://mongo:27017")
 	mdb, err = mongo.Connect(context.Background(), mdbOpts)
 	if err != nil {
@@ -80,6 +97,19 @@ func run() (err error) {
 	// Handle SIGINT (CTRL+C) gracefully.
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer stop()
+
+	// Set up OpenTelemetry.
+	otelShutdown, err := setupOTelSDK(ctx)
+	if err != nil {
+		return
+	}
+	// Handle shutdown properly so nothing leaks.
+	defer func() {
+		err = errors.Join(err, otelShutdown(context.Background()))
+	}()
+
+	// initialize tracer
+	tracer = otel.Tracer(os.Getenv("OTEL_SERVICE_NAME"))
 
 	// Start HTTP server.
 	srv := &http.Server{
@@ -116,7 +146,17 @@ func newHTTPHandler() http.Handler {
 	// handleFunc is a replacement for mux.HandleFunc
 	// which enriches the handler's HTTP instrumentation.
 	handleFunc := func(pattern string, handlerFunc func(http.ResponseWriter, *http.Request)) {
-		mux.HandleFunc(pattern, handlerFunc)
+		// Configure the "http.route" for the HTTP instrumentation.
+		handler := otelhttp.WithRouteTag(
+			pattern, http.HandlerFunc(handlerFunc),
+		)
+		handler = otelhttp.NewHandler(
+			handler, pattern,
+			otelhttp.WithSpanNameFormatter(func(operation string, r *http.Request) string {
+				return r.Method + " " + operation
+			}),
+		)
+		mux.Handle(pattern, handler)
 	}
 
 	// Register handlers.
@@ -177,16 +217,38 @@ func mongoFunc(w http.ResponseWriter, r *http.Request) {
 }
 
 func clickhouseFunc(w http.ResponseWriter, r *http.Request) {
+	_, span := tracer.Start(r.Context(), "SELECT <dbname>.<tablename>", apiTrace.WithSpanKind(apiTrace.SpanKindClient))
+	span.SetAttributes(
+		semconv.DBSystemClickhouse,
+		// semconv.DBName(""),
+		semconv.DBOperation("SELECT"),
+		// semconv.DBSQLTable(""),
+		// semconv.DBStatement(""),
+	)
+	defer span.End()
 
 	res, err := ccn.Query(r.Context(), "SELECT NOW()")
 	if err != nil {
-		fmt.Fprintf(w, "Clickhouse query error: %v", err)
+		span.RecordError(err)
+		span.SetStatus(codes.Error, err.Error())
 	}
 
 	fmt.Fprintf(w, "Clickhouse called: %v", res.Columns())
 }
 
 func kafkaProduceFunc(w http.ResponseWriter, r *http.Request) {
+	_, span := tracer.Start(r.Context(), "publish "+kafkaTopicName, apiTrace.WithSpanKind(apiTrace.SpanKindProducer))
+	span.SetAttributes(
+		semconv.MessagingSystemKafka,
+		semconv.MessagingOperationPublish,
+		semconv.MessagingDestinationName(kafkaTopicName),
+		// semconv.ServerAddress(""),
+		// semconv.MessagingKafkaMessageKey(""),
+		// semconv.MessagingMessageBodySize(14),
+		// semconv.MessagingBatchMessageCount(3),
+		// attribute.String("key", "value"), // "go.opentelemetry.io/otel/attribute"
+	)
+	defer span.End()
 
 	kcn.SetWriteDeadline(time.Now().Add(10 * time.Second))
 	_, err := kcn.WriteMessages(
@@ -195,13 +257,21 @@ func kafkaProduceFunc(w http.ResponseWriter, r *http.Request) {
 		kafka.Message{Value: []byte("three!")},
 	)
 	if err != nil {
-		fmt.Fprintf(w, "Kafka produce error: %v", err)
+		span.RecordError(err)
+		span.SetStatus(codes.Error, err.Error())
 	}
 
 	fmt.Fprintf(w, "Kafka produced")
 }
 
 func kafkaConsumeFunc(w http.ResponseWriter, r *http.Request) {
+	_, span := tracer.Start(r.Context(), "process "+kafkaTopicName, apiTrace.WithSpanKind(apiTrace.SpanKindConsumer))
+	span.SetAttributes(
+		semconv.MessagingSystemKafka,
+		semconv.MessagingOperationDeliver,
+		semconv.MessagingDestinationName(kafkaTopicName),
+	)
+	defer span.End()
 
 	kcn.SetReadDeadline(time.Now().Add(10 * time.Second))
 	_ = kcn.ReadBatch(10e3, 1e6) // fetch 10KB min, 1MB max
